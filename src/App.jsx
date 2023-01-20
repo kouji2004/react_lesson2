b4077346f1f5ed05a920cbfdae773e97d286e7ed
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-20 16:47:37 +0900

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useReducer } from 'react';
 import { useEffect, useState, useContext, useRef} from 'react';
 import './App.css'
 import ShinCodeContext from './main';
+
+const reducer = (state, action)=>{
+  switch(action,type){
+    case "increment":
+      return state+1;
+      case "decrement":
+        return state-1;
+        default:
+          return state;
+  }
+}
+
 
 function App() {
  const [count,setCount] = useState(0);
   const shinCodeInfo = useContext(ShinCodeContext);
   const ref =useRef();
-
+  const [state, dispatch] = useReducer(reducer,0);
 
  const handleClick=()=>{
   setCount(count+1);
@@ -35,6 +48,12 @@ function App() {
       <h1>useRef</h1>
       <input type="text" ref={ref}/>
       <button onClick={handleRef}>UseRef</button>
+
+      <hr/>
+      <h1>useReducer</h1>
+      <p>カウント:{state}</p>
+      <button onClick={()=>dispatch({type: "increment"})}>+</button>
+      <button onClick={()=>dispatch({type: "decrement"})}>-</button>
 
     </div>
 

6a742eb44acf8276505c954153b10522c6fbe362
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-20 13:35:40 +0900

    UseState

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import './App.css'
 
 function App() {
   const count =0;
+ const handleClick = () =>{
+  count++;
+ }
 
   return (
     <div className="App">
       <h1>UseState</h1>
+      <button onClick={handleClick}>+</button>
+      <p>{count}</p>
     </div>
   )
 }

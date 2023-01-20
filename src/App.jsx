e06493949ada3f71970a69604ffb8f5a9cba4166
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-20 13:38:47 +0900

    UseState ボタンをクリックするとカウント

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const count =0;
- const handleClick = () =>{
-  count++;
+ const [count,setCount] = useState(0);
+  
+ const handleClick=()=>{
+  setCount(count+1);
  }
 
   return (
